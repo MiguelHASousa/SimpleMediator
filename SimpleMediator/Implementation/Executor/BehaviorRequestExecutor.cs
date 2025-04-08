83fb2b9a868f59db5179d940a46e6536d51e8640
Miguel Sousa <miguelsousa@git.com.br>
2025-04-07 21:15:20 -0300

    feat: Adiciona behaviorRequestExecutor

---
+++ SimpleMediator/Implementation/Executor/BehaviorRequestExecutor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimpleMediator.Core.Implementation.Delegate;
+using SimpleMediator.Core.Interfaces.Executors;
+using SimpleMediator.Core.Interfaces.PipelineBehavior;
+using SimpleMediator.Core.Interfaces.Request;
+
+namespace SimpleMediator.Core.Implementation.Executor;
+
+public class BehaviorRequestExecutor(IServiceProvider provider) : IRequestExecutor
+{
+    public async Task<TResponse> Execute<TRequest, TResponse>(
+        TRequest request,
+        IRequestHandler<TRequest, TResponse> handler
+    )
+    where TRequest : IRequest<TResponse>
+    {
+        var behaviors = provider
+            .GetServices<IPipelineBehavior<TRequest, TResponse>>()
+            .Reverse()
+            .ToList();
+
+        RequestHandlerDelegate<TResponse> next = () => handler.Handle(request);
+
+        foreach (var behavior in behaviors)
+        {
+            var current = next;
+            next = () => behavior.Handle(request, current);
+        }
+
+        return await next();
+    }
+}
