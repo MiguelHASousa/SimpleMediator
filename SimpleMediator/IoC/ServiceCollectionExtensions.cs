70ac6f3194e370de8ff22bf39ddfed1147c4653d
Miguel Sousa <miguelsousa@git.com.br>
2025-04-08 21:44:01 -0300

    feat: Adiciona Extension para IoC custom

---
+++ SimpleMediator/IoC/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SimpleMediator.Core.Abstractions.Executors;
+using SimpleMediator.Core.Abstractions.HandlerResolver;
+using SimpleMediator.Core.Abstractions.Mediator;
+using SimpleMediator.Core.Infrastructure.Executor;
+using SimpleMediator.Core.Infrastructure.Mediator;
+using SimpleMediator.Core.Infrastructure.ServiceProviderHandler;
+
+namespace SimpleMediator.Core.IoC;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSimpleMediator(this IServiceCollection services)
+    {
+        services.TryAddScoped<IHandlerResolver, ServiceProviderHandlerResolver>();
+        services.TryAddScoped<IRequestExecutor, BehaviorRequestExecutor>();
+        services.TryAddScoped<INotificationExecutor, NotificationExecutor>();
+        services.TryAddScoped<IMediator, Mediator>();
+
+        return services;
+    }
+}
